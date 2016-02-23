a433d89aa825442c3ece96f7e85746a2779571fa
Chris Anderson <cl.anderson8@gmail.com>
2016-02-22 18:04:57 -0800

    fun with learning about unit testing

---
+++ Mathmagician.Tests/IntegerTests.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Mathmagician.Tests
+{
+    [TestClass]
+    public class IntegerTests
+    {
+        [TestMethod]
+        public void IntegerEnsureICanCreateAnInstance()
+        {
+            Integer my_int = new Integer();
+            //if red squiggly, check if you have a class in the main project. Also check accessibility levels if squigs persist.
+            Assert.IsNotNull(my_int);
+            //creating an instance, and ensure it's not null. This is step one / sanity check.
+        }
+
+        [TestMethod]
+        public void IntegerEnsureICanGetFirstNumber()
+        {
+            //Arrange : section 1. Scenario setup
+            Integer my_int = new Integer();
+
+            //Act: Section 2. do the thing you wanna test
+            int actual = my_int.GetFirst();
+            int expected = 0;
+
+            //Assert: section 3. Did it work as expected?
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void EnsureICanGetNextInteger()
+        {
+            //Arrange
+            Integer my_int = new Integer();
+            //Act
+            int actual = my_int.GetNext(5);
+            int expected = 6;
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void IntegerEnsureICanCreateASequenceOfTenIntegers()
+        {
+            //Arrange
+            Integer my_int = new Integer();
+
+            //Act
+            int[] actual = my_int.GetSequence(10);
+            int[] expected = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            //Assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void IntegerEnsureMaxIsNotExceeded()
+        {
+            //Arrange
+            Integer my_int = new Integer();
+
+            //Act
+            int[] actual = my_int.GetSequence(my_int.MAX+1);
+
+            //Assert
+
+            //below uses an older testing framework
+            //Assert.Throws(typeof(Exception), my_int.GetSequence(my_int.MAX));
+        }
+    }
+}
---
+++ Mathmagician/Integer.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mathmagician
+{
+    public class Integer
+    {
+
+        public int MAX { get; private set; } 
+
+        /*
+        
+            using properties is like 
+            private int max = 50;
+            public int GetMax()
+            {
+                return this.max;
+            }
+
+            private void SetMax(int new_max
+            {
+                this.max = new_max;
+            }
+
+            */
+
+
+
+        public Integer()
+        {
+            MAX = 50;
+        }
+
+        public int GetFirst()
+        {
+            return 0;
+        }
+
+        public int GetNext(int current)
+        {
+            return current + 1;
+        }
+
+        public int[] GetSequence(int how_many)
+        {
+            if (how_many > MAX)
+            {
+                throw new Exception();
+            }
+
+            int[] seq_array = new int[how_many];
+            //^this (int[how_many]) makes sure the array has this much space in the array
+            
+            /* //this is one way to do this - passes:
+            for (int i = 0; i < how_many; i++)
+            {
+                seq_array[i] = i;
+            }*/
+
+            int counter = 0;
+            seq_array[counter] = GetFirst();
+            while (counter < how_many -1);
+            {
+                /*attempt 1
+                seq_array[counter+1] = GetNext(seq_array[counter]);
+                counter++;*/
+
+                counter++;
+                seq_array[counter] = GetNext(seq_array[counter - 1]);  
+            }
+
+            return seq_array;
+        }
+    }
+}
